4a0c2b34f679dd93f9f03f29fa8b96b2a252cf7f
lingjiangj <44994488+lingjiangj@users.noreply.github.com>
2018-11-28 09:58:53 +0800

    Update ps5-Smallest multiple.py

--- ps5-Smallest multiple.py
+++ ps5-Smallest multiple.py
@@ -1,3 +1,7 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+# find prime factors of numbers from 20 to 1 and create dictionary for the frequency of each prime factor.
+
+# 
