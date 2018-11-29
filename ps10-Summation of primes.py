3207a501e240045fcecab95dd280b19a29d57df1
lingjiangj <44994488+lingjiangj@users.noreply.github.com>
2018-11-29 18:07:24 +0800

    Create ps10-Summation of primes.py

---
+++ ps10-Summation of primes.py
@@ -0,0 +1,27 @@
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+# Find the sum of all the primes below two million.
+
+
+import math
+def isPrime(num):
+    if num <= 1:
+        return False
+    for i in range(2,int(math.sqrt(num))+1):
+        if num % i == 0:
+            return False
+    return True
+
+def sumPrime(n):
+    """
+    n is the upper limit
+    """
+    total = 0
+    for i in range(2,n+1):
+        if isPrime(i) == True:
+            total += i
+            
+    return total
+
+
+print(sumPrime(2000000))
