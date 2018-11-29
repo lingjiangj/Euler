f4ba087d82619cc860770a0ea1c0fd1f19571b33
lingjiangj <44994488+lingjiangj@users.noreply.github.com>
2018-11-29 18:20:15 +0800

    Update p20-Factorial digit sum.py

--- p20-Factorial digit sum.py
+++ p20-Factorial digit sum.py
@@ -4,3 +4,19 @@
 # and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 # Find the sum of the digits in the number 100!
+
+def fact(n):
+    if n == 1:
+        return 1
+    else:
+        return n*fact(n-1)
+
+def factsum(n):
+    
+    num = fact(n)
+    strnum = str(num)
+    result = sum(int(strnum[i]) for i in range(len(strnum)))
+    
+    return result
+
+print(factsum(100))
